7a616eb55a81791913ee8553097ce928f9671cce
Orhys Brown <51386750+orhys@users.noreply.github.com>
2019-12-22 19:50:54 -0500

    added image upload functionality

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,10 +1,11 @@
 const admin = require("firebase-admin");
 
+// Required to run Admin SDK functions locally
 const serviceAccount = require("../../social-ape-e4a2a-firebase-adminsdk-cb0n5-785ac604b9.json");
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://social-ape-e4a2a.firebaseio.com"
+  databaseURL: "https://social-ape-e4a2a.firebaseio.com",
+  storageBucket: "social-ape-e4a2a.appspot.com"
 });
 
 const db = admin.firestore();
