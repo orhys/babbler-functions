7bbb723edfd038f19256162362c9d93dd26deec0
Orhys Brown <51386750+orhys@users.noreply.github.com>
2019-12-30 19:42:05 -0500

    completed notiication route

--- functions/util/admin.js
+++ functions/util/admin.js
@@ -1,15 +1,14 @@
 const admin = require("firebase-admin");
 
-admin.initializeApp();
+// admin.initializeApp();
 
 // Required to run Firebase Admin SDK functions locally
-// const serviceAccount = require("../../social-ape-e4a2a-firebase-adminsdk-cb0n5-785ac604b9.json");
-// admin.initializeApp({
-//     credential: admin.credential.cert(serviceAccount),
-//     databaseURL: "https://social-ape-e4a2a.firebaseio.com",
-//     storageBucket: "social-ape-e4a2a.appspot.com"
-// });
-
+const serviceAccount = require("../../social-ape-e4a2a-firebase-adminsdk-cb0n5-785ac604b9.json");
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://social-ape-e4a2a.firebaseio.com",
+  storageBucket: "social-ape-e4a2a.appspot.com"
+});
 
 const db = admin.firestore();
 
